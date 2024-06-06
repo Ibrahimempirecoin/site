d4962f89c3b3f7440bd99dd3d5f9f699aa249a71
Ibrahimempirecoin <ibrahimsamirgashimov@gmail.com>
2024-06-06 21:09:25 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     var tiktokLink = document.getElementById("tiktok-link");
+    var playButton = document.getElementById("play-button");
 
-    tiktokLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        window.open("https://www.tiktok.com/@empirecoin?_t=8mfv3gKGH0T&_r=1", "_blank");
-    });
+    if (tiktokLink) {
+        tiktokLink.addEventListener("click", function(event) {
+            event.preventDefault();
+            window.open("https://www.tiktok.com/@empirecoin?_t=8mfv3gKGH0T&_r=1", "_blank");
+        });
+    }
+
+    if (playButton) {
+        playButton.addEventListener("click", function() {
+            window.location.href = "https://t.me/Empirecoin_bot";
+        });
+    }
 });
