d0ce1e61294878416d35660d38c79038c971d2c0
Ibrahimempirecoin <ibrahimsamirgashimov@gmail.com>
2024-06-12 16:51:25 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (playButton) {
         playButton.addEventListener("click", function() {
-            window.location.href = "https://t.me/Empirecoin";
+            window.location.href = "";
         });
     }
 });
